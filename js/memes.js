22bbfa83b3559cd9e89484ed1b42dd291958b971
Joseph Barten <josbarten@wl.k12.ia.us>
2018-10-31 13:37:38 -0500

    memes

--- js/memes.js
+++ js/memes.js
@@ -1,3 +1,146 @@
 var memes = [
-    
-];
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:["Dank memes"],
+        product_id:"1001"
+    },
+    {
+        image:"D-Rose.jpg",
+        title:"Break Bones",
+        categories:["Dank memes"],
+        product_id:"1002"
+    },
+    {
+        image:"Fatty.jpg",
+        title:"fat person",
+        categories:["Dank memes"],
+        product_id:"1003"
+    },
+    {
+        image:"Grillin.jpg",
+        title:"steak",
+        categories:["Dank memes"],
+        product_id:"1004"
+    },
+    {
+        image:"Tony-Stark.png",
+        title:"salt dude",
+        categories:["Dank memes"],
+        product_id:"1005"
+    },
+    {
+        image:"Buzz.jpg",
+        title:"moth",
+        categories:["Dank memes","Classic"],
+        product_id:"1006"
+    },
+    {
+        image:"Futureaa.jpg",
+        title:"Futurama",
+        categories:["Dank memes","Classic"],
+        product_id:"1007"
+    },
+    {
+        image:"Kermit.jpg",
+        title:"kermit frog",
+        categories:"Dank memes","Classic",
+        product_id:"1008"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+    {
+        image:"Bro-you-even-lift.png",
+        title:"jacked bird",
+        categories:"Dank memes",
+        product_id:"1001"
+    },
+],
