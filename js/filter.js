332828ac09e574120ff74aba05423a00cbbbffc2
Dylan Gunn <dylgunn@wl.k12.ia.us>
2018-11-05 14:52:15 -0600

    found memes

---
+++ js/filter.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function setUpCategoryFilter() {
+    var categories;
+    var categoryList = [];
+    $('.product').each(function() {
+        categories = $(this).attr('data-categories').split(',');
+        categories.forEach(function(category) {
+            if(categoryList.indexOf(category) < 0) {
+                categoryList.push(category);
+            }
+        });
+    });
+    categoryList.sort();
+    categoryList.forEach(function(category) {
+        var optionTag = `<option value="${category}">${category}</option>`;
+        $('#category-filter').append(optionTag);
+    });
+};
+
+function handleCategoryFilter() {
+    $('#category-filter').on('change', function() {
+        if($(this).val()) {
+            $('.meme').hide();
+            $(`.meme[data-categories*="${$(this).val()}"]`).fadeIn();
+        }
+        else {
+            $('.meme').fadeIn();
+        }
+    });
+};
