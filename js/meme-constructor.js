61f35b501901553b2c533d923b2f01bef95be52b
Joseph Barten <josbarten@wl.k12.ia.us>
2018-11-06 15:22:18 -0600

    add stuff to the constructor file

--- js/meme-constructor.js
+++ js/meme-constructor.js
@@ -3,5 +3,22 @@
 var meme_list = [];
 
 var Meme = function(meme){
-    this.image = Bro-you-even-lift.png;
-}
+    this.image = meme.image;
+   this.title = meme.title;
+    this.categories = meme.categories;
+    this.meme_id = meme.meme_id;
+};
+
+Meme.prototype.toHtml = function(){
+    var source = $('#meme-template').text();
+    var template = Handlebars.compile(source);
+    return template(this);
+};
+
+memes.foreach(function(meme){
+    meme_list.push(new Meme(meme));
+});
+
+meme_list.foreach(function(meme){
+    $('#product-listings').append(meme.toHtml());
+});
